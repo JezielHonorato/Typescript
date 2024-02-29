4fe4184fcc821189304cd4a9d5d5f57ce128f731
Jeziel Honorato <136933391+JezielHonorato@users.noreply.github.com>
2024-02-29 10:43:15 -0300

    innitial commit

---
+++ teste.js
@@ -0,0 +1,6 @@
+"use strict";
+const ex = 15;
+const ex2 = 30;
+const ex3 = 41;
+let consol = (ex * ex2 * ex3);
+console.log(consol);
---
+++ teste.ts
@@ -0,0 +1,8 @@
+const ex: number = 15;
+
+const ex2: number = 30;
+const ex3: number = 41;
+
+let consol = (ex * ex2 * ex3);
+
+console.log(consol);
