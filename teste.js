2bfd91cadda72bc0f5068f64c1d6f1054941e2f8
Jeziel Honorato <136933391+JezielHonorato@users.noreply.github.com>
2024-02-29 11:51:34 -0300

    att

--- teste.js
+++ teste.js
@@ -1,6 +1,26 @@
 "use strict";
-const ex = 15;
-const ex2 = 30;
-const ex3 = 41;
-let consol = (ex * ex2 * ex3);
-console.log(consol);
+let disciplinas = ['ppi', 'so', 'pds', 'esp'];
+console.log(disciplinas);
+console.log(`quantidade: ${disciplinas.length}`);
+disciplinas.push('hist'); //add fim
+disciplinas.unshift('qui'); //add começo
+// disciplinas.pop() //tira ultimo
+console.log(disciplinas);
+disciplinas.splice(0, 3); //retira oq ta na posição 1 e o prox
+console.log(disciplinas);
+let cursos = new Array(4);
+cursos[0] = 'informatica';
+console.log(cursos);
+var Curso;
+(function (Curso) {
+    Curso["INFO"] = "informatica";
+    Curso["MECA"] = "mecatronica";
+    Curso["REDES"] = "redes de computadores";
+})(Curso || (Curso = {}));
+let estudante = {
+    nome: 'sibele',
+    matricula: 20211144010069,
+    curso: Curso.INFO,
+    ano: 4
+};
+console.log(estudante);
--- teste.ts
+++ teste.ts
@@ -1,8 +1,28 @@
-const ex: number = 15;
+let disciplinas =  ['ppi', 'so', 'pds', 'esp']
+    console.log(disciplinas)
+    console.log(`quantidade: ${disciplinas.length}`)
+    disciplinas.push('hist') //add fim
+    disciplinas.unshift('qui') //add começo
+// disciplinas.pop() //tira ultimo
+    console.log(disciplinas)
+    disciplinas.splice(0, 3) //retira oq ta na posição 1 e o prox
+    console.log(disciplinas)
 
-const ex2: number = 30;
-const ex3: number = 41;
+let cursos = new Array(4)
+cursos[0] = 'informatica'
+console.log(cursos)
 
-let consol = (ex * ex2 * ex3);
+enum Curso {    
+    INFO = 'informatica',
+    MECA =  'mecatronica',
+    REDES = 'redes de computadores'
+}
 
-console.log(consol);
+let estudante = {
+    nome: 'sibele',
+    matricula: 20211144010069,
+    curso: Curso.INFO, 
+    ano: 4
+}
+
+console.log(estudante)
